e25c2fde5213eb16d91733e872ce6d9f60e7894e
sugumar123456 <128585244+sugumar123456@users.noreply.github.com>
2023-08-23 12:29:49 +0530

    Add files via upload

---
+++ binary search method.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid; 
+        } else if (arr[mid] < target) {
+            left = mid + 1; 
+        } else {
+            right = mid - 1; 
+        }
+    }
+
+    return -1; 
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 23;
+
+    int index = binarySearch(arr, size, target);
+
+    if (index != -1) {
+        printf("Target %d found at index %d\n", target, index);
+    } else {
+        printf("Target %d not found in the array\n", target);
+    }
+
+    return 0;
+}
